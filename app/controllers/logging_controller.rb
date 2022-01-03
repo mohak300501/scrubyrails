1a9eb649c387fe2a6b813e6c949f2187736681f2
mohak300501 <mohak_k@ph.iitr.ac.in>
2022-01-03 15:08:32 +0530

    'integrate'

--- app/controllers/logging_controller.rb
+++ app/controllers/logging_controller.rb
@@ -1,5 +1,6 @@
 class LoggingController < ApplicationController
     def login
+        @test = :password
         if :password == "namaskar"
             session[:user] ||= "sc"
             redirect_to shloka_ashlokar_path
