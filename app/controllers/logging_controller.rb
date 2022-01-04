5adc4550d9e6e6e72657d8a87304cb7dde056ae2
mohak300501 <mohak_k@ph.iitr.ac.in>
2022-01-04 22:34:00 +0530

    'integrate'

--- app/controllers/logging_controller.rb
+++ app/controllers/logging_controller.rb
@@ -1,14 +1,18 @@
 class LoggingController < ApplicationController
     def login
+    end
+    def loging
         unless Member.find_by(email: params[:email]).nil?
             if Member.find_by(email: params[:email]).password == params[:password]
                 session[:user] = "sc"
                 redirect_to shloka_ashlokar_path
             else
                 flash.now[:notice] = "Password does not match"
+                render login
             end
         else
             flash.now[:notice] = "Email not found"
+            render login
         end
     end
     def logout
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'welcome#home'
   get "logging/login"
+  post "logging/loging"
   get "logging/logout"
-  post "logging/login"
   get "shloka/rshloka"
   get "shloka/ashlokar"
   get "events/revents"
