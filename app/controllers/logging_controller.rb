2f94ac4187c5db3c350efe79292ef1679d47d60e
mohak300501 <mohak_k@ph.iitr.ac.in>
2022-01-04 22:09:41 +0530

    'integrate'

--- app/controllers/logging_controller.rb
+++ app/controllers/logging_controller.rb
@@ -1,15 +1,14 @@
 class LoggingController < ApplicationController
     def login
-        @mssg = nil
         if Member.find_by(email: params[:email])
             if Member.find_by(email: params[:email]).password == params[:password]
                 session[:user] = "sc"
                 redirect_to shloka_ashlokar_path
             else
-                @mssg = "Password does not match"
+                flash.now[:notice] = "Password does not match"
             end
         else
-            @mssg = "Email not found"
+            flash.now[:notice] = "Email not found"
         end
     end
     def logout
