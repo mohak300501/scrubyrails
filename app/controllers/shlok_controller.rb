af40756633a0396f9b1ae29b87ae08aa4a7210c7
mohak300501 <mohak_k@ph.iitr.ac.in>
2022-01-05 00:04:07 +0530

    'integrate'

--- app/controllers/shlok_controller.rb
+++ app/controllers/shlok_controller.rb
@@ -1,29 +1,30 @@
 class ShlokaController < ApplicationController
-    def ashlokar
+    def ashlokr
         if session[:admin]
-            render "ashlokar"
+            render "ashlokr"
         else
             redirect_to root_url
         end
     end
 
-    def ashlokac
+    def ashlokc
         if session[:admin]
-            render "ashlokac"
+            render "ashlokc"
         else
             redirect_to root_url
         end
     end
 
-    def ashlokacp
+    def ashlokcp
+        shlok = Shlok.new(:title => params[:title], :shlok => params[:shlok], :translation => [:translation])
         flash[:notice] = "नूतनः श्लोकः योजितः जातः!"
-        redirect_to shloka_ashlokar_path
+        redirect_to shlok_ashlokr_path
     end
 
-    def ashlokau
+    def ashloku
     end
 
-    def ashlokad
+    def ashlokd
         unless session[:admin]
             redirect_to root_url
         end
