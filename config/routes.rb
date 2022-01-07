8d99029459c501dff68ca1771f4eef03161eccdd
mohak300501 <mohak_k@ph.iitr.ac.in>
2022-01-07 11:36:34 +0530

    'integrate'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     root 'welcome#home'
+    get "welcome/profile", as:"profile"
 
     get "logging/mlogin", as:"member_login"
     get "logging/ulogin", as:"user_login"
