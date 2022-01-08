523fb3ab8b80a6c9f9b9a48ea66bbcf87a199861
mohak300501 <mohak_k@ph.iitr.ac.in>
2022-01-08 13:41:15 +0530

    'integrate;

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
-    before_action :visit_c
-    def visit_c
+    before_action :visit_count
+    def visit_count
         session[:visit] ||= 0
         session[:visit] += 1
         if session[:visit] == 1
             visits = Visit.first
             vc = visits.vid
             visits.update(:vid => vc)
-            @visit_count = Visit.first.vid
+            session[:vc] = Visit.first.vid
         end
     end
 end
