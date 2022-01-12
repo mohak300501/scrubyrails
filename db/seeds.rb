dd8f273b0b4e811edb6ddf26060a7b2843a381fd
mohak300501 <mohak_k@ph.iitr.ac.in>
2022-01-12 18:25:35 +0530

    'integrate'

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Visit.create(vid: 0)
+User.create(name: 'user1', email: 'user1@gmail.com', password: 'user1pass')
+Member.create(name: 'Sanskrit Club', email: 'sanskritclub@iitr.ac.in', password: 'namaskar')
