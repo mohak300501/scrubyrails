759008f308b8d619811cbf2cf5cce413223657bf
rahulgummaluri212 <91544700+rahulgummaluri212@users.noreply.github.com>
2022-01-20 22:05:48 +0530

    'commit6'

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,4 +2,11 @@ class WelcomeController < ApplicationController
     def home
         @visit = session[:visit]
     end
+    def mhomer
+        if session[:member]
+            render "mhomer"
+        else
+            redirect_to root_url
+        end
+    end
 end
