8375e62e6011a6a121f3224fcb8456e35fc7fa94
mohak300501 <mohak_k@ph.iitr.ac.in>
2022-04-08 13:42:20 +0530

    integrate

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -23,11 +23,18 @@ class CourseController < ApplicationController
 
     def mcoursecp
         course = Course.new(:name => params[:name], :description => params[:description], :image => params[:image])
-                            
         course.save
+
+        ActiveRecord::Base.connection.execute('create table ' + params[:name] +
+            '(id serial primary key, pid int foreign key references users(id), email varchar(50) foreign key references users(email));')
+
         flash[:notice] = "course will be displayed"
         redirect_to all_courses_path
     end
 
-
+    def mcoursedp
+        course = Course.find(params[:id])
+        course.destroy
+        redirect_to all_courses_path
+    end
 end
