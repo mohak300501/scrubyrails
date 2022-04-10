6eab5aa4e9243bb480cce55f878d04e17c0ddecb
mohak300501 <mohak_k@ph.iitr.ac.in>
2022-04-10 22:40:30 +0530

    integrate

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -36,7 +36,7 @@ class CourseController < ApplicationController
     def mcoursedp
         course = Course.find(params[:id])
         hello = course.name
-        ActiveRecord::Base.connection.execute('drop table ' + course.name + ' if exists;')
+        ActiveRecord::Base.connection.execute('drop table if exists ' + course.name + ';')
         course.destroy
         flash[:notice] = "hi" + hello
         redirect_to all_courses_path
