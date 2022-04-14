95b3f17f3574e480fae3fa63175ad0e8929d8808
saisriharsha286 <85404999+saisriharsha286@users.noreply.github.com>
2022-04-14 10:58:47 +0530

    ann

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,13 @@
 class WelcomeController < ApplicationController
     def home
         @news = Announcement.all
+        @shloks = Shlok.all
         @visit = session[:visit]
     end
     def mhomer
         if session[:member]
             @news = Announcement.all
+            @shloks = Shlok.all
             render "mhomer"
         else
             redirect_to root_url
