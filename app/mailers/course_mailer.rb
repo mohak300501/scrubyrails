461e22b378b2f3b1bcd1ed51dab392c93dec6237
mohak300501 <mohak_k@ph.iitr.ac.in>
2022-04-15 20:25:37 +0530

    integrate

--- app/mailers/course_mailer.rb
+++ app/mailers/course_mailer.rb
@@ -2,6 +2,6 @@ class CourseMailer < ApplicationMailer
     def ureg_mail
         @course = params[:course]
         user = params[:user_email]
-        mail(to: user, subject: "Successfully registered for course " + course + ".")
+        mail(to: user, subject: "Successfully registered for course " + @course + ".")
     end
 end
