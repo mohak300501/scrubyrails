25afbd43c6c5d430ad1b1e07a56ba0f34affbd1a
mohak300501 <mohak_k@ph.iitr.ac.in>
2022-04-16 11:35:47 +0530

    integrate

--- app/mailers/otp_mailer.rb
+++ app/mailers/otp_mailer.rb
@@ -2,6 +2,6 @@ class OtpMailer < ApplicationMailer
     def otp_mail
         @otp = params[:otp]
         email = params[:email]
-        mail(to: email, subject: "OTP for siging up on Sanskrit Club IITR website.")
+        mail(to: email, subject: "OTP for signing up on Sanskrit Club IITR website.")
     end
 end
