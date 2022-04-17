ec1b838ce1b1e74f07fd9e338dd1790816a1f16d
mohak300501 <mohak_k@ph.iitr.ac.in>
2022-04-17 22:40:40 +0530

    integragte

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-    scope :filter_by_country, -> (country) { where('country like ?', '%country%') }
-    scope :filter_by_state, -> (state) { where('state like ?', '%state%') }
-    scope :filter_by_pin, -> (pin) { where('pin like ?', '%pin%') }
-    scope :filter_by_gender, -> (gender) { where('gender like ?', '%gender%') }
-    scope :filter_by_age, -> (age) { where('age like ?', '%age%') }
-    scope :filter_by_sanslevel, -> (sanslevel) { where('sanslevel like ?', '%sanslevel%') }
-    scope :filter_by_acadqual, -> (acadqual) { where('acadqual like ?', '%acadqual%') }
+    scope :filter_by_country, -> (country) { where('country like ?', '%{country}%') }
+    scope :filter_by_state, -> (state) { where('state like ?', '%{state}%') }
+    scope :filter_by_pin, -> (pin) { where('pin like ?', '%{pin}%') }
+    scope :filter_by_gender, -> (gender) { where('gender like ?', '%{gender}%') }
+    scope :filter_by_age, -> (age) { where('age like ?', '%{age}%') }
+    scope :filter_by_sanslevel, -> (sanslevel) { where('sanslevel like ?', '%{sanslevel}%') }
+    scope :filter_by_acadqual, -> (acadqual) { where('acadqual like ?', '%{acadqual}%') }
 end
