10019c5c8bc285738b319ef8b0e3e58f17dfc478
mohak300501 <mohak_k@ph.iitr.ac.in>
2022-04-18 01:23:01 +0530

    integragte

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -55,13 +55,13 @@ class UserController < ApplicationController
         if session[:member]
             if request.post?
                 @user = User
-                @user = User.find_by(country: params[:country]) if params[:country].present?
                 @user = @user.filter_by_state(params[:state]) if params[:state].present?
                 @user = @user.filter_by_pin(params[:pin]) if params[:pin].present?
                 @user = @user.filter_by_gender(params[:gender]) if params[:gender].present?
                 @user = @user.filter_by_age(params[:age]) if params[:age].present?
                 @user = @user.filter_by_sanslevel(params[:sanslevel]) if params[:sanslevel].present?
                 @user = @user.filter_by_acadqual(params[:acadqual]) if params[:acadqual].present?
+                @user = User.find_by(country: params[:country]) if params[:country].present?
                 render "users"
             else
                 @users = User.all
