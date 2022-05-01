7a499e3e0a697bbd1726cf91fc55a0859e5f5a6e
mohak300501 <mohak_k@ph.iitr.ac.in>
2022-05-01 18:25:50 +0530

    integragte

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    scope :filter_by_, lambda {|key, value| where('? like ?', key, '%'+value+'%') }
+    scope :filter_by_, lambda {|key, value| where('? like ?', key, value) }
     # scope :filter_by_state, -> (state) { where('state like ?', '%{state}%') }
     # scope :filter_by_pin, -> (pin) { where('pin like ?', '%{pin}%') }
     # scope :filter_by_gender, -> (gender) { where('gender like ?', '%{gender}%') }
