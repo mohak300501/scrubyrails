f867858b6307e37956c318f5ea10ffcd09d3c0ae
mohak300501 <mohak_k@ph.iitr.ac.in>
2022-05-07 14:17:53 +0530

    integragte

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -36,16 +36,18 @@ class CourseController < ApplicationController
     def mcourseu
         if session[:member]
             @course = Course.find(params[:id])
-            if !(@course.cname == params[:cname])
-                ActiveRecord::Base.connection.execute("alter table " + @course.cname + " rename to " + params[:cname] + ";")
+            if request.post?
+                if !(@course.cname == params[:cname])
+                    ActiveRecord::Base.connection.execute("alter table " + @course.cname + " rename to " + params[:cname] + ";")
+                end
+                @course.update(:name => params[:name], :cname => params[:cname], :description => params[:description])
+                if params[:image].present?
+                    @course.image.purge
+                    @course.image.attach(params[:image])
+                end
+                flash[:notice] = "CourseH परिवर्तितः जातः!"
+                redirect_to all_courses_path
             end
-            @course.update(:name => params[:name], :cname => params[:cname], :description => params[:description])
-            if params[:image].present?
-                @course.image.purge
-                @course.image.attach(params[:image])
-            end
-            flash[:notice] = "CourseH परिवर्तितः जातः!"
-            redirect_to all_courses_path
         else
             redirect_to root_url
         end
