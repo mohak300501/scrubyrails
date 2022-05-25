720e4d82c8b33a099a4ff072c0250649178fde16
rahulgummaluri212 <91544700+rahulgummaluri212@users.noreply.github.com>
2022-05-25 13:48:25 +0530

    'j'

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -2,4 +2,8 @@ class FeedbackController < ApplicationController
     def ufeedbackp
         redirect_to feedback_path
     end
+    def ufeedback
+        @feedbacks = Feedback.all
+        render "ufeedback"
+    end
 end
