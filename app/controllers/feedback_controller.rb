8ed19d5084ba5a49fcf65e0aa7eec74c7a48e54c
rahulgummaluri212 <91544700+rahulgummaluri212@users.noreply.github.com>
2022-05-26 17:18:51 +0530

    'f'

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -2,7 +2,7 @@ class FeedbackController < ApplicationController
     def ufeedbackp
         @feedback = Feedback.new(:email => params[:email], :type_of_feedback => params[:type_of_feedback], 
         :feedback => params[:feedback])
-        feedback.save
+        @feedback.save
         redirect_to feedback_path
     end
     def ufeedback
